a1443aa2bf1cb1fc39039e6bf8bf69a596c84de0
Eiko Ujifusa <eikorouji@gmail.com>
2022-04-13 10:04:50 -0700

    added thought controller routes

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -34,17 +34,82 @@ const thoughtController = {
         },
 
         // create new thought //
+        createThought({ params, body }, res) {
+            Thought.create(body)
+              .then(({ _id }) => {
+                  return.user.findOneAndUpdate(
+                      { userName: body.username },
+                      { $push: { thoughts: _id } },
+                      { new: true }
+                  );
+              })
+              .then(dbUserData => {
+                  res.status(404).json({ message: 'No user found with this username :('});
+                  return;
+              }
+              res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+        },
         
-        // update thought // 
+        // update thought by ID // 
+        updateThought({ params, body }, res) {
+            Thought.findOneAndUpdate(
+                { _id: params.id },
+                body,
+                { new: true, runValidators: true }
+            )
+            .then(updatedThought => {
+                if (!updatedThought) {
+                    return res.status(404).json({ message: 'No thought with this ID' });
+                }
+                res.json(updatedThought);
+            })
+            .catch(err => res.json(err));
+            },
+        },
 
         // delete thought //
-        
+        deleteThought({ params, body}, res) {
+            Thought.findOneAndDelete({ _id: params.id })
+              .then(deletedThought => {
+                  if (!deletedThought) {
+                      return res.status(404).json({ message: 'No thought with this ID!'})
+                  } 
+                  res.json(deletedThought);
+              })
+              .catch(err => res.json(err));
+        }
+    };
+
 
         // add reaction //
+        addReaction ({ params, body}, res) {
+            Thought.findOneAndUpdate(
+                { _id: params.thoughtId },
+                { $push: { reactions: body } },
+                { new: true, runValidators: true }
+            )
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought with this Id'});
+                    return;
+                }
+                res.json(dbThoughtData)
+            })
+            .catch(err => res.json(err));
+        },
 
         // delete reaction //
+        removeReaction({ params }, res) {
+            Thought.findOneAndUpdate(
+                { _id: params.thoughtId },
+                { $pull: {reactions: { reactionId: params.reactionId}}},
+                { new: true }
+            )
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => res.json(err));
+        }
+};
 
-
- 
-    }
-}
+module.exports = thoughtController
